92b9df700eb1f14a9266ebb890a67d2a5cd80041
HP_VICTUS_CGM92\cleyb <cgarcia@playdigitalconsulting.com>
2024-10-17 23:23:07 +0200

    Actualizamos el repo

--- main.go
+++ main.go
@@ -7,16 +7,13 @@ import (
 )
 
 func main() {
-	Go := &course.Course{
-		Name:    "GO desde Cero",
-		Price:   12.34,
-		IsFree:  false,
-		UserIDs: []uint{12, 56, 89},
-		Classes: map[uint]string{
-			1: "Introducción",
-			2: "Estructuras",
-			3: "Maps",
-		},
+	Go := course.New("Go desde cero", 0, false)
+
+	Go.UserIDs = []uint{12, 56, 89}
+	Go.Classes = map[uint]string{
+		1: "Introducción",
+		2: "Estructuras",
+		3: "Maps",
 	}
 
 	Go.ChangePrice(24.99)
